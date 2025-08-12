0e5a0bb09a93db3340b649d6538c923487e5b991
cadyngo <156823740+cadyngo@users.noreply.github.com>
2025-08-12 06:19:18 +0000

    Deploying to gh-pages from @ cadyngo/cadyngo.github.io@51fc88edc217eefc3d5668a4b9450e4963d49431 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "About",
+    id: "nav-research",
+    title: "Research",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -14,14 +14,14 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/publications/";
+            window.location.href = "/";
           },
         },{id: "nav-cv",
           title: "CV",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/";
           },
         },{id: "books-the-godfather",
           title: 'The Godfather',
